466d95e91ce1e6100af0838ca5b48ecff367699c
fandaL <lofff@email.cz>
2017-05-08 18:38:14 +0200

    update example 1

--- beso_conf.py
+++ beso_conf.py
@@ -6,33 +6,18 @@ path = "."  # path to the working directory where the initial file is located
 #path = "."  # example - in the current working directory
 #path = "~/tmp/beso/"  # Linux example
 #path = "D:\\tmp\\"  # Windows example
-path_calculix = "d:\\soft\FreeCad\\FreeCAD_0.17.8264_x64_dev_win\\bin\\ccx"  # path to the CalculiX solver
+path_calculix = "d:\\soft\FreeCad\\FreeCAD_0.17.10993_x64_dev_win\\bin\\ccx"  # path to the CalculiX solver
 #path_calculix = "/usr/bin/ccx"  # Linux example, may help shell command: which ccx
 #path_calculix = "d:\\soft\FreeCad\\FreeCAD_0.17.8264_x64_dev_win\\bin\\ccx"  # Windows example
 
-file_name = "Fusion_Mesh.inp"  # file with prepared linear static analysis
+file_name = "Plane_Mesh.inp"  # file with prepared linear static analysis
 
-elset_name = "SolidMaterialShellThickness"  # string with name of the element set in .inp file
+elset_name = "SolidMaterialElementGeometry2D"  # string with name of the element set in .inp file
 domain_optimized[elset_name] = True  # True - optimized domain, False - elements will not be removed
 domain_density[elset_name] = [1e-6, 1]  # equivalent density of the domain material for states of switch_elm
 domain_thickness[elset_name] = [1.0, 1.0]  # thickness of shell elements for states of switch_elm
 domain_offset[elset_name] = 0.0  # offset of shell elements
-domain_FI[elset_name] = [[("stress_von_Mises", 450.0)],  # inner tuples () for separate Failure indices
-                         [("stress_von_Mises", 450.0)]]  # new inner list [] for the next state of switch_elm
-                        # Filure Indices definition in python tuples (separeate FI for each element state if there are more lists)
-                        # Failure Indice FI = element stress / allowable value
-                        # examples:
-                        # [("user_def", "sxx / 600.0"), ("user_def", "syy / 150.0"), ("user_def", "sxy / 50.0")]  # "user_def" defines complete formula for FI
-                        # [("stress_von_Mises", 450.0)]  # for von Mises stress give only allowable stress
-domain_material[elset_name] = ["*ELASTIC \n210000e-6,  0.3",  # material definition after CalculiX *MATERIAL card, use \n for line break
-                               "*ELASTIC \n210000,  0.3"]  # next string for the next state of switch_elm
-
-elset_name = "SolidMaterial001ShellThickness"  # string with name of the element set in .inp file
-domain_optimized[elset_name] = False  # True - optimized domain, False - elements will not be removed
-domain_density[elset_name] = [7.9e-15, 7.9e-9]  # equivalent density of the domain material for states of switch_elm
-domain_thickness[elset_name] = [1.0, 1.0]  # thickness of shell elements for states of switch_elm
-domain_offset[elset_name] = 0.0  # offset of shell elements
-domain_FI[elset_name] = [[("stress_von_Mises", 450.0)],  # inner tuples () for separate Failure indices
+domain_FI[elset_name] = [[("stress_von_Mises", 450.0e6)],  # inner tuples () for separate Failure indices
                          [("stress_von_Mises", 450.0)]]  # new inner list [] for the next state of switch_elm
                         # Filure Indices definition in python tuples (separeate FI for each element state if there are more lists)
                         # Failure Indice FI = element stress / allowable value
@@ -48,7 +33,7 @@ mass_goal_ratio = 0.4  # the goal mass as a fragment of the optimized domains fu
 continue_from = ""  # if not "", optimization will load full elements from the given files,
                               #  use 0th file name e.g. "file051_res_mesh0.frd" or "file051_res_mesh0.inp"
 
-filter_list = [["close sensitivity", 1.5]]  # [[filter type, range, domains or nothing for all domains], [next filter type, range, "domain1", "domain2"], ...]
+filter_list = [["simple", 2]]  # [[filter type, range, domains or nothing for all domains], [next filter type, range, "domain1", "domain2"], ...]
                             # filter types:
                             # "simple" - averages sensitivity number with surroundings (suffer from boundary sticking?), works on sensitivities
                             # morphology based filters:
@@ -78,10 +63,10 @@ decay_coefficient = -0.2  # exponential decay coefficient to dump mass_additive_
                            # exp(-0.22 * x) ~ drops after 10 iterations to 0.1
                            # exp(0 * x) ~ no decaying
 
-shells_as_composite = True  # True - use more integration points to catch bending stresses (ccx 2.12 WILL FAIL for other than S4R and S6 shell elements)
+shells_as_composite = False  # True - use more integration points to catch bending stresses (ccx 2.12 WILL FAIL for other than S8R and S6 shell elements)
                             # False - use ordinary shell section
 reference_points = "integration points"  # "integration points" - read int. pt values from .dat file,
-                                         # "nodes" - optimization will read nodal values from .inp file (model MUST NOT contain shell or beam elements)
+                                         # "nodes" - optimization will read nodal values from .inp file (model MUST NOT contain shell nor beam elements)
 reference_value = "max"  # "max" - maximal value on element,
                         # "average" - average value on element (do not use for bended shell elements)
 sensitivity_averaging = False  # True - averaging sensitivity numbers with previous iteration, False - do not average
@@ -94,4 +79,4 @@ iterations_limit = 0  # 0 - automatic estimate, <integer> - the maximum allowabl
 tolerance = 1e-3  # the maximum relative difference in mean stress in optimization domains between the last 5 iterations needed to finish
 
 save_iteration_results = 10  # every i-th iteration export a resulting mesh and do not delete .frd and .dat, 0 - do not save
-save_iteration_format = "frd" # "frd" or "inp" format of resulting meshes (each state separately in own mesh file)
+save_iteration_format = "inp" # "frd" or "inp" format of resulting meshes (each state separately in own mesh file)
